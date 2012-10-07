847ab840b0f3c26d4faf6bebf8276ba857c7a665
Katsuyoshi Ito <kito@itosoft.com>
2012-10-07 10:26:37 +0900

    staging environment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :password_protected if Rails.env.staging?
+
+  protected
+
+  def password_protected
+    authenticate_or_request_with_http_basic do |username, password|
+      username == "digicommu" && password == "akita"
+    end
+  end
+
 end
