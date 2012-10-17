5a299a3315237c6ed26c6da17446be52ad180bb0
Katsuyoshi Ito <kito@itosoft.com>
2012-10-17 16:25:37 +0900

    申し込み状況追加

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,3 +1,5 @@
+require 'open-uri'
+
 class WelcomeController < ApplicationController
   def index
     @articles = Article.all
@@ -5,6 +7,12 @@ class WelcomeController < ApplicationController
     @chairperson = Staff.chairperson
     @staffs = Staff.staffs
     @contributors = Staff.contributors
+
+    @registration_status = begin
+      a = JSON.parse(open('http://api.doorkeeper.jp/groups/digicommu-akita/events.json').read)
+      a.each.map{|e| {title:e['event']['title'], participants:e['event']['participants'], ticket_limit:e['event']['ticket_limit']}}.sort_by{|e| e[:title].length}
+    rescue Exception => e
+    end
   end
 
   def access
