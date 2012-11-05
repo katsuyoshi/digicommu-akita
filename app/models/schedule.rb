89dbacad7245c690efd826b9097eaac692bf9a55
Katsuyoshi Ito <kito@itosoft.com>
2012-11-05 23:20:36 +0900

    FEJ秋田に変更

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -10,7 +10,7 @@ class Schedule < FileBaseModel
   end
 
   def self.fandroid_akita
-    self.new organization:"Fandroid EAST JAPAN 秋田支部", organization_tmplate:'fandroid_akita'
+    self.new organization:"FEJ秋田", organization_tmplate:'fandroid_akita'
   end
 
   attr_accessor :start, :end, :title, :kind, :abstract_template, :abstract, :organization_tmplate, :organization
