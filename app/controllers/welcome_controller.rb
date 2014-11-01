6e672d58cdce12044e63cc8e4b1a3ca17398da3d
Katsuyoshi Ito <kito@itosoft.com>
2014-11-01 09:03:22 +0900

    セッション内容を表示できる様にした

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,4 +2,13 @@ class WelcomeController < ApplicationController
   def index
     @timelines = YAML.load File.read(Rails.root + "db/timeline_2014.yml")
   end
+  
+  def show
+    @slot = params[:slot]
+    @room_name = params[:room_name]
+    index = params[:index].to_i
+    timelines = YAML.load File.read(Rails.root + "db/timeline_2014.yml")
+    @presentation = timelines.find{|t| t["slot"] == @slot}[@room_name]["presentations"][index]
+  end
+  
 end
